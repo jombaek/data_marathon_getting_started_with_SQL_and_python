42851327884d2ae53f9fb81acf1be17343315c73
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-08-10 23:57:20 +0700

    5.2.1

---
+++ 5.2.1.py
@@ -0,0 +1,8 @@
+avia_ticket_price = 13400
+avia_pass_qnt = 2
+hotel_night_price = 2100
+hotel_night_qnt = 6
+car_rent_daily_price = 4500
+car_rent_days_qnt = 3
+
+print(avia_ticket_price * avia_pass_qnt + hotel_night_price * hotel_night_qnt + car_rent_daily_price * car_rent_days_qnt)
